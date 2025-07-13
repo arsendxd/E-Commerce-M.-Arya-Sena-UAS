c7ed5cdbf73663c597f6c8c74ba9794da72809bf
M. Arya Sena <aryasena8879@gmail.com>
2025-07-13 19:23:16 +0700

    cart.js

---
+++ JS/cart.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const keranjang = JSON.parse(localStorage.getItem("keranjang")) || [];
+    const totalJumlah = keranjang.reduce((acc, item) => acc + item.jumlah, 0);
+    const cartLink = document.querySelector(".cart-link");
+    if (cartLink) {
+      cartLink.textContent = `🛒 (${totalJumlah})`;
+    }
+  });
+  
